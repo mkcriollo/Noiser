06fd21783370216aae38d797e0526bcfb422b667
Mike Criollo <mkcriollo@gmail.com>
2021-01-28 19:15:38 -0500

    seed edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,61 +50,61 @@ user10 = User.create!(email: "sheckwes@gmail.com", password: "password123",usern
 
 
 #59
-song1 = Song.create!(title:"Tequila Shots", genre: "Rap", description: "Written Kid Cudi", artist_id: 103)
+song1 = Song.create!(title:"Tequila Shots", genre: "Rap", description: "Written Kid Cudi", artist_id: 1)
 song1_art = URI.open("https://noiser-seed.s3.amazonaws.com/tequilashots.jpg");
 song1.photo.attach(io: song1_art, filename: 'tequilashots.jpg')
 song1_audio = URI.open("https://noiser-seed.s3.amazonaws.com/TequilaShots.mp3");
 song1.song.attach(io: song1_audio, filename: 'TequilaShots.mp3')
 #61
-song2 = Song.create!(title:"Wake Up", genre: "Rap", description: "Written by Travis Scott", artist_id: 104)
+song2 = Song.create!(title:"Wake Up", genre: "Rap", description: "Written by Travis Scott", artist_id: 2)
 song2_art = URI.open("https://noiser-seed.s3.amazonaws.com/wakeup.jpeg")
 song2.photo.attach(io: song2_art, filename: 'wakeup.jpeg')
 song2_audio = URI.open("https://noiser-seed.s3.amazonaws.com/WAKEUP.mp3");
 song2.song.attach(io: song2_audio, filename: 'WAKEUP.mp3')
 #62
-song3 = Song.create!(title:"Praise The Lord", genre: "Rap", description: "Written by Asap Rocky", artist_id: 105)
+song3 = Song.create!(title:"Praise The Lord", genre: "Rap", description: "Written by Asap Rocky", artist_id: 3)
 song3_art = URI.open("https://noiser-seed.s3.amazonaws.com/praisethelord.png")
 song3.photo.attach(io: song3_art, filename: 'praisethelord.png')
 song3_audio = URI.open("https://noiser-seed.s3.amazonaws.com/PraiseTheLord.mp3");
 song3.song.attach(io: song3_audio, filename: 'PraiseTheLord.mp3')
 #63
-song4 = Song.create!(title:"Roll in Peace", genre: "Rap", description: "Written by Kodak Black & XXXTentacion", artist_id: 106)
+song4 = Song.create!(title:"Roll in Peace", genre: "Rap", description: "Written by Kodak Black & XXXTentacion", artist_id: 4)
 song4_art = URI.open("https://noiser-seed.s3.amazonaws.com/rollinpeace.jpg")
 song4.photo.attach(io: song4_art, filename: 'rollinpeace.jpg')
 song4_audio = URI.open("https://noiser-seed.s3.amazonaws.com/RollinPeace.mp3");
 song4.song.attach(io: song4_audio, filename: 'RollinPeace.mp3')
 #64
-song5 = Song.create!(title:"Rigamortus", genre: "Rap", description: "Written by Kendrick Lamar", artist_id: 107)
+song5 = Song.create!(title:"Rigamortus", genre: "Rap", description: "Written by Kendrick Lamar", artist_id: 5)
 song5_art = URI.open("https://noiser-seed.s3.amazonaws.com/rigamortis.jpg")
 song5.photo.attach(io: song5_art, filename: 'rigamortis.jpg')
 song5_audio = URI.open("https://noiser-seed.s3.amazonaws.com/Rigamortus.mp3");
 song5.song.attach(io: song5_audio, filename: 'Rigamortus.mp3')
 #65
-song6 = Song.create!(title:"Topanga", genre: "Rap", description: "Witten by Trippe Red", artist_id: 108)
+song6 = Song.create!(title:"Topanga", genre: "Rap", description: "Witten by Trippe Red", artist_id: 6)
 song6_art = URI.open("https://noiser-seed.s3.amazonaws.com/topanga.jpeg")
 song6.photo.attach(io: song6_art, filename: 'topanga.jpeg')
 song6_audio = URI.open("https://noiser-seed.s3.amazonaws.com/Topanga.mp3");
 song6.song.attach(io: song6_audio, filename: 'Topanga.mp3')
 #66
-song7 = Song.create!(title:"Tookie Knows 11", genre: "Rap", description: "Written By SchoolBoy Q", artist_id: 109)
+song7 = Song.create!(title:"Tookie Knows 11", genre: "Rap", description: "Written By SchoolBoy Q", artist_id: 7)
 song7_art = URI.open("https://noiser-seed.s3.amazonaws.com/tookieknows.jpg")
 song7.photo.attach(io: song7_art, filename: 'tookieknows.jpg')
 song7_audio = URI.open("https://noiser-seed.s3.amazonaws.com/TookieKnowsII.mp3");
 song7.song.attach(io: song7_audio, filename: 'TookieKnowsII.mp3')
 #67
-song8 = Song.create!(title:"Heavenly Father", genre: "Rap", description: "Written by Isaiah Rashad", artist_id: 110)
+song8 = Song.create!(title:"Heavenly Father", genre: "Rap", description: "Written by Isaiah Rashad", artist_id: 8)
 song8_art = URI.open("https://noiser-seed.s3.amazonaws.com/heavenlyfather.jpg")
 song8.photo.attach(io: song8_art, filename: 'heavenlyfather.jpg')
 song8_audio = URI.open("https://noiser-seed.s3.amazonaws.com/HeavenlyFather.mp3");
 song8.song.attach(io: song8_audio, filename: 'Heavenly+Father.mp3')
 #68
-song9 = Song.create!(title:"Power", genre: "Rap", description: "Written by Kanye West", artist_id: 111)
+song9 = Song.create!(title:"Power", genre: "Rap", description: "Written by Kanye West", artist_id: 9)
 song9_art = URI.open("https://noiser-seed.s3.amazonaws.com/power.png")
 song9.photo.attach(io: song9_art, filename: 'power.png')
 song9_audio = URI.open("https://noiser-seed.s3.amazonaws.com/POWER.mp3");
 song9.song.attach(io: song9_audio, filename: 'POWER.mp3')
 #69
-song10 = Song.create!(title:"BEENBALLIN", genre: "Rap", description: "Written by Sheck Wes", artist_id: 112)
+song10 = Song.create!(title:"BEENBALLIN", genre: "Rap", description: "Written by Sheck Wes", artist_id: 10)
 song10_art = URI.open("https://noiser-seed.s3.amazonaws.com/beenballing.png")
 song10.photo.attach(io: song10_art, filename: 'beenballing.png')
 song10_audio = URI.open("https://noiser-seed.s3.amazonaws.com/BeenBallin.mp3");
